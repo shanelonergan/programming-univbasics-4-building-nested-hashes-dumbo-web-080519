c5d64c134788b891717380bfc8f54f3b2bf19718
Shane Lonergan <sptlonergan@gmail.com>
2019-07-25 16:09:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,15 +15,15 @@ def monopoly
 				:reading_railroad =>
 				{
 					"mortgage_value" => "$100"
-				}
+				},
 				:pennsylvania_railroad =>
 				{
 					"mortgage_value" => "$200"
-				}
+				},
 				:b_and_o_railroad =>
 				{
 					"mortgage_value" => "$200"
-				}
+				},
 				:shortline =>
 				{
 					"mortgage_value" => "$800"
