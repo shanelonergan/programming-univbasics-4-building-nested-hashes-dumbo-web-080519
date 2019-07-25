afc44200c4865f0053111119484cdc7e38f0cb14
Shane Lonergan <sptlonergan@gmail.com>
2019-07-25 16:07:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,28 +3,28 @@ def monopoly
 		:railroads =>
 		{
 			pieces: 4,
-			:rent_in_dollars => 
+			:rent_in_dollars =>
 			{
 				one_piece_owned: 25,
 				two_pieces_owned: 50,
 				three_pieces_owned: 100,
 				four_pieces_owned: 200,
 			},
-			:names => 
+			:names =>
 			{
 				:reading_railroad =>
 				{
 					"mortgage_value" => "$100"
 				}
-				:pennsylvania_railroad => 
+				:pennsylvania_railroad =>
 				{
 					"mortgage_value" => "$200"
 				}
-				:b_and_o_railroad => 
+				:b_and_o_railroad =>
 				{
 					"mortgage_value" => "$200"
 				}
-				:shortline => 
+				:shortline =>
 				{
 					"mortgage_value" => "$800"
 				}
